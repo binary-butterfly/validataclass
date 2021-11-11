11a38d3f5c01c5ee9edb02b4834171cdec65602e
Lexi Stelter <lexi.stelter@binary-butterfly.de>
2021-11-11 18:26:57 +0100

    ValidataclassMixin: Remove UnsetValues in to_dict() unless keep_unset_values is true

--- tests/helpers/dataclass_mixins_test.py
+++ tests/helpers/dataclass_mixins_test.py
@@ -41,7 +41,7 @@ class ValidataclassMixinTest:
     def test_validataclass_to_dict_validated():
         """ Tests the to_dict() method of the ValidataclassMixin class using a DataclassValidator. """
         validator = DataclassValidator(UnitTestDataclass)
-        obj = validator.validate({'foo': 42, 'bar': 'meep', 'baz': '-1.23'})
+        obj: UnitTestDataclass = validator.validate({'foo': 42, 'bar': 'meep', 'baz': '-1.23'})
         assert obj.to_dict() == {
             'foo': 42,
             'bar': 'meep',
@@ -52,8 +52,18 @@ class ValidataclassMixinTest:
     def test_validataclass_to_dict_validated_with_defaults():
         """ Tests the to_dict() method of the ValidataclassMixin class using a DataclassValidator, with default values. """
         validator = DataclassValidator(UnitTestDataclass)
-        obj = validator.validate({'foo': 42})
-        obj_as_dict = obj.to_dict()
+        obj: UnitTestDataclass = validator.validate({'foo': 42})
+        assert obj.to_dict() == {
+            'foo': 42,
+            'bar': 'bloop',
+        }
+
+    @staticmethod
+    def test_validataclass_to_dict_validated_keep_unset_values():
+        """ Tests the to_dict() method of the ValidataclassMixin class with the parameter keep_unset_value=True. """
+        validator = DataclassValidator(UnitTestDataclass)
+        obj: UnitTestDataclass = validator.validate({'foo': 42})
+        obj_as_dict = obj.to_dict(keep_unset_values=True)
         assert obj_as_dict == {
             'foo': 42,
             'bar': 'bloop',
@@ -69,6 +79,10 @@ class ValidataclassMixinTest:
         obj = UnitTestDataclass.create_with_defaults(foo=42)
         assert isinstance(obj, UnitTestDataclass)
         assert obj.to_dict() == {
+            'foo': 42,
+            'bar': 'bloop',
+        }
+        assert obj.to_dict(keep_unset_values=True) == {
             'foo': 42,
             'bar': 'bloop',
             'baz': UnsetValue,
@@ -101,9 +115,13 @@ class ValidataclassMixinTest:
             foo: int = Default(3)
             bar: Optional[str] = Default(None)
 
-        obj = UnitTestSubclass.create_with_defaults()
+        obj: UnitTestSubclass = UnitTestSubclass.create_with_defaults()
         assert isinstance(obj, UnitTestSubclass)
         assert obj.to_dict() == {
+            'foo': 3,
+            'bar': None,
+        }
+        assert obj.to_dict(keep_unset_values=True) == {
             'foo': 3,
             'bar': None,
             'baz': UnsetValue,
